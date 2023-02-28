16f3f8901c54486e91143a2be912c3fd15b5c750
Veer Kunwer Bharti <veer7549kr@gmail.com>
2023-02-28 14:55:23 +0530

    Added navbar component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,62 @@
+"use client"
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import {
+  FaHome,
+  FaUser,
+  FaBriefcase,
+  FaPhone,
+  FaRocketchat,
+} from "react-icons/fa";
+import styles from "./navbar.module.css";
+
+const Navbar = () => {
+  const pathname = usePathname();
+  console.log(pathname);
+
+  return (
+    <nav className={styles.navbar}>
+      <ul className={styles.navList}>
+        <li>
+          <Link href="/" className={pathname == "/" ? styles.active : ""}>
+            <FaHome />
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/about"
+            className={pathname == "/about" ? styles.active : ""}
+          >
+            <FaUser />
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/portfolio"
+            className={pathname == "/portfolio" ? styles.active : ""}
+          >
+            <FaBriefcase />
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/contact"
+            className={pathname == "/contact" ? styles.active : ""}
+          >
+            <FaPhone />
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/blogs"
+            className={pathname == "/blogs" ? styles.active : ""}
+          >
+            <FaRocketchat />
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
