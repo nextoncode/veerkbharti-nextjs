ff0a524793910c2ad5706834173ef4b21d30d1f0
Veer Kunwer Bharti <veer7549kr@gmail.com>
2023-02-28 14:55:05 +0530

    Setup project

---
+++ src/app/head.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Head = () => {
+  return (
+    <>
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Veer Kunwer Bharti</title>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+      />
+    </>
+  );
+};
+
+export default Head;
