22b037f2d820a743d2e1f0b83f964c7401a75150
Veer Kunwer Bharti <veer7549kr@gmail.com>
2023-03-02 01:23:24 +0530

    Added contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,101 @@
+import SectionMainTitle from "@/components/Titles/SectionMainTitle";
+import Link from "next/link";
+import {
+  FaGithubAlt,
+  FaInstagram,
+  FaLinkedinIn,
+  FaPhone,
+  FaRocketchat,
+  FaYoutube,
+} from "react-icons/fa";
+import styles from "./contact.module.css";
+
 const ContactPage = () => {
   return (
-    <div>ContactPage</div>
-  )
-}
+    <section>
+      <div className="container">
+        <SectionMainTitle mainTitle="Contact" subTitle="Get in touch" />
 
-export default ContactPage
+        <div>
+          <div className="row">
+            <div className="col-sm-6">
+              <div className={styles.contactInfoBox}>
+                <h4>Don't be shy</h4>
+                <p>
+                  Feel free to get in touch with me. I am always open to
+                  discussing new projects, creative ideas or opportunities to be
+                  part of your visions.
+                </p>
+                <ul className={styles.contactInfo}>
+                  <li>
+                    <span>
+                      <FaRocketchat />
+                    </span>
+                    <p>
+                      Mail me <br />{" "}
+                      <Link href={"mailto:veerkbharti@gmail.com"}>
+                        veerkbharti@gmail.com
+                      </Link>
+                    </p>
+                  </li>
+                  <li>
+                    <span>
+                      <FaPhone />
+                    </span>
+                    <p>
+                      Call me <br />{" "}
+                      <Link href={"tel:+919471988414"}>
+                        +91-9471988414
+                      </Link>
+                    </p>
+                  </li>
+                </ul>
+                <ul className={styles.socialLinks}>
+                  <li>
+                    <Link href={"#"}>
+                      <FaLinkedinIn />
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href={"#"}>
+                      <FaGithubAlt />
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href={"#"}>
+                      <FaYoutube />
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href={"#"}>
+                      <FaInstagram />
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-sm-1"></div>
+            <div className="col-sm-5">
+              <div className={styles.contactForm}>
+                <form>
+                  <input type="text" name="name" id="name" placeholder="Name" />
+                  <input type="email" name="email" id="email" placeholder="Email" />
+                  <textarea
+                    name="message"
+                    id="message"
+                    cols="30"
+                    rows="4"
+                    placeholder="Message"
+                  ></textarea>
+                  <button type="submit" className="btn">Send Message</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ContactPage;
